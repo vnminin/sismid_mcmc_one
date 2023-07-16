82f76c74aca7bb68a51ea95f69ef0c006fa1b921
vnminin <vminin@uci.edu>
2023-07-16 14:48:49 -0700

    added Volodymyr reduced scripts

---
+++ 2023/code/diagnostics_reduced.R
@@ -0,0 +1,68 @@
+## This script illustrates some diagnostic tools available in package coda
+## Author: Volodymyr M. Minin
+## last update: 07/16/2023
+
+## first we need to load coda and mcmcse packages (you need to install them first)
+library(coda)
+library(mcmcse)
+
+## now let's load our M-H and Gibbs sampler examples
+source("https://raw.githubusercontent.com/vnminin/sismid_mcmc_one/master/2022/code/chainGibbs.R")
+dev.off()
+
+## run one M-H example chain
+gibbs_chain1 = chainGibbs(5000, 1,1)
+
+## convert the output into coda format
+coda_gibbs_chain = mcmc(cbind(gibbs_chain1$q[101:5000],gibbs_chain1$n111[101:5000]))
+
+summary(coda_gibbs_chain)
+
+## we can also compute Monte Carlo error for the quantiles
+mcse.q.mat(coda_gibbs_chain, 0.025)
+mcse.q.mat(coda_gibbs_chain, 0.975)
+
+## plot traceplots and posterior densities
+plot(coda_gibbs_chain)
+
+## look at what coda can do
+help(package=coda)
+
+## look at the menu options
+##codamenu()
+
+## all commands are availabe outside of the menu
+
+## plot autocorrelations plots
+autocorr.plot(coda_gibbs_chain)
+
+## calculate effective sample size
+effectiveSize(coda_gibbs_chain)
+
+
+## Run 50 chains with overdispersed starting values
+coda_gibbs_chains = list()
+
+for (i in 1:50){
+  gibbs_chain = chainGibbsUserStart(1000,1,1,sample(0:275,size=1))
+  coda_gibbs_chains[[i]] = mcmc(cbind(gibbs_chain$q,gibbs_chain$n111))
+}
+
+coda_gibbs_list = mcmc.list(coda_gibbs_chains)
+
+plot(coda_gibbs_list)
+
+## compute and plot Gelman-Rubin potential reduction factor
+gelman.diag(coda_gibbs_list)
+
+gelman.plot(coda_gibbs_list)
+
+
+## Exercise: perform diagnostics for SIR data augmentatkon MCMC
+
+source("https://raw.githubusercontent.com/vnminin/sismid_mcmc_one/master/2023/code/SIRaugmentation.R")
+
+## calling the above script produced mcmc.sample matrix 
+
+coda_sir_chain = mcmc(mcmc.sample) 
+
